46ee365daf01fb97e02a468252cf1d89fc7f7fdb
SimonTanner <simonthetanner@hotmail.com>
2021-06-15 15:24:51 +0100

    Tides api

--- api/api.go
+++ api/api.go
@@ -21,9 +21,7 @@ type Api struct {
 func NewApi(redCli redis.Client) Api {
 	router := mux.NewRouter()
 	router.HandleFunc("/message/{key}", GetMessage).Methods("GET")
-	router.HandleFunc("/add/{key}/{message}", SetMessage).Methods("GET")
-	router.HandleFunc("/", GetMessages).Methods("GET")
-	router.HandleFunc("/home", HomePage).Methods("GET", "POST")
+	router.HandleFunc("/", HomePage).Methods("GET", "POST")
 
 	api := Api{
 		Router:      router,
@@ -34,7 +32,6 @@ func NewApi(redCli redis.Client) Api {
 }
 
 func GetMessage(w http.ResponseWriter, r *http.Request) {
-
 	redisClient := redis.NewClient(redis.RedisConf{
 		Address: addr,
 	})
@@ -51,42 +48,6 @@ func GetMessage(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		jsvals, _ := json.Marshal(map[string]string{key: val})
 		w.Write(jsvals)
-	}
-}
-
-func GetMessages(w http.ResponseWriter, r *http.Request) {
-	redisClient := redis.NewClient(redis.RedisConf{
-		Address: addr,
-	})
-
-	vals, err := redisClient.GetAll(r.Context())
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		if len(vals) == 0 {
-			fmt.Println("no messages found")
-			w.WriteHeader(http.StatusNoContent)
-		} else {
-			w.WriteHeader(http.StatusOK)
-			jsvals, _ := json.Marshal(vals)
-			w.Write(jsvals)
-		}
-	}
-}
-
-func SetMessage(w http.ResponseWriter, r *http.Request) {
-	redisClient := redis.NewClient(redis.RedisConf{
-		Address: addr,
-	})
-
-	vars := mux.Vars(r)
-	fmt.Println(vars["key"])
-	fmt.Println(vars["message"])
-
-	if err := redisClient.Set(r.Context(), vars["key"], vars["message"]); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else {
-		w.WriteHeader(http.StatusOK)
 	}
 }
 
